d80153b0e4f58c8881d6e98ea3bcecefd66187fd
Kelmfaker <kelmfaker@gmail.com>
2024-10-06 07:59:32 +0000

    pascal triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,17 +1,13 @@
 def pascal_triangle(n):
     if n <= 0:
-       return []
-    triangle = []
-    for row in range(n):
-        current_row = []
-        for col in range(row + 1):
-            if col == 0 or col == row:
-                current_row.append(1)
-            else:
-                current_row.append(triangle[row - 1][col - 1] + triangle[row - 1][col])
-        triangle.append(current_row)
-    return triangle
+        return []
 
-def display_pascals_triangle(triangle):
-    for row in triangle:
-        print(" ".join(map(str, row)))
+    triangle = [[1]]
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i-1][j-1] + triangle[i-1][j])
+        row.append(1)
+        triangle.append(row)
+    
+    return triangle
