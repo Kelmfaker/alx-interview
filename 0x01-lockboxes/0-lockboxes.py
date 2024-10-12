5da2a7a989f2e1bc15805a2cc18e25847f3a640c
Kelmfaker <kelmfaker@gmail.com>
2024-10-12 14:39:52 -0700

    lock boxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""
+function determine if all boxs can be opened
+"""
+
+
+from typing import List
+
+def canUnlockAll(boxes: List[List[int]]) -> bool:
+    """
+    return
+    true: if all boxes can be opend
+    false: if not
+    """
+    n = len(boxes)
+    opened = set([0])  # Start with the first box
+    keys = list(boxes[0])  # Start with keys from the first box
+
+    while keys:
+        key = keys.pop()  # Remove and return the last key
+        if key < n and key not in opened:
+            opened.add(key)
+            keys.extend(boxes[key])  # Add new keys found in the box
+
+    return len(opened) == n  # Return True if all boxes are opened, else False
