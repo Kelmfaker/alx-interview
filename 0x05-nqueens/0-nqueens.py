d895700e263325e043f80e5f519f767e0ab4b3a9
Kelmfaker <kelmfaker@gmail.com>
2024-11-10 11:40:58 +0000

    N queens

---
+++ 0x05-nqueens/0-nqueens.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+"""
+This module solves the N queens problem.
+"""
+
+import sys
+
+
+def print_solution(board):
+    """
+    Print the board in the required format.
+    """
+    solution = []
+    for row in range(len(board)):
+        for col in range(len(board)):
+            if board[row][col] == 1:
+                solution.append([row, col])
+    print(solution)
+
+
+def is_safe(board, row, col):
+    """
+    Check if it's safe to place a queen at board[row][col].
+    """
+    for i in range(col):
+        if board[row][i] == 1:
+            return False
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+    for i, j in zip(range(row, len(board), 1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+    return True
+
+
+def solve_nqueens_util(board, col):
+    """
+    Utilizes backtracking to solve the N queens problem.
+    """
+    if col >= len(board):
+        print_solution(board)
+        return True
+    res = False
+    for i in range(len(board)):
+        if is_safe(board, i, col):
+            board[i][col] = 1
+            res = solve_nqueens_util(board, col + 1) or res
+            board[i][col] = 0
+    return res
+
+
+def solve_nqueens(n):
+    """
+    Solve the N queens problem for a given n.
+    """
+    board = [[0 for _ in range(n)] for _ in range(n)]
+    if not solve_nqueens_util(board, 0):
+        print("No solution exists")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    try:
+        n = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+    if n < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+    solve_nqueens(n)
